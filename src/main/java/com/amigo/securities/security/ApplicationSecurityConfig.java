73153fcfe922b586dbeb271105fdcc25f239dfdd
lucian <lucian@mail.com>
2020-10-02 15:42:42 +0700

    使用PasswordEncoder 对创建的用户密码进行编码

--- src/main/java/com/amigo/securities/security/ApplicationSecurityConfig.java
+++ src/main/java/com/amigo/securities/security/ApplicationSecurityConfig.java
@@ -1,13 +1,28 @@
 package com.amigo.securities.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // 表示 任何请求都需要验证 并且 使用 basic auth
@@ -19,5 +34,19 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
             .authenticated()
             .and()
             .httpBasic();
+    }
+
+    // 用于从数据库中获取用户
+    @Override
+    @Bean // 用于SpringBoot帮助我们自动注入
+    protected UserDetailsService userDetailsService() {
+        UserDetails kobeBryant = User.builder()
+                .username("kobebryant")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT") // spring 会将roles转换成 ROLE_STUDENT
+                .build();
+
+        return new InMemoryUserDetailsManager(kobeBryant);
+
     }
 }
---
+++ src/main/java/com/amigo/securities/security/PasswordConfig.java
@@ -0,0 +1,16 @@
+package com.amigo.securities.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // BCryptPasswordEncoder是PasswordEncoder的一种实现
+        return new BCryptPasswordEncoder(10);
+    }
+}

15521f62b9bb9ce8021edd165cc6e43df229c850
lucian <lucian@mail.com>
2020-10-02 13:47:28 +0700

    使用WebSecurityConfigurerAdapter 实现basic auth

---
+++ src/main/java/com/amigo/securities/security/ApplicationSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.amigo.securities.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 表示 任何请求都需要验证 并且 使用 basic auth
+        http
+            .authorizeRequests()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .httpBasic();
+    }
+}

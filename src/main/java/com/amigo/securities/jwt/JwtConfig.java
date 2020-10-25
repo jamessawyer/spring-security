fe4e4dd85a9b08188ce60a00cc3a344d4743de39
lucian <lucian@mail.com>
2020-10-25 23:03:31 +0700

    5.将jwt一些常量定义为配置的形式

---
+++ src/main/java/com/amigo/securities/jwt/JwtConfig.java
@@ -0,0 +1,23 @@
+package com.amigo.securities.jwt;
+
+import com.google.common.net.HttpHeaders;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+// 使用 @ConfigurationProperties 可能报错
+// https://stackoverflow.com/a/57950415
+@Getter
+@Setter
+@NoArgsConstructor
+@ConfigurationProperties(prefix = "application.jwt")
+public class JwtConfig {
+    private String secretKey;
+    private String tokenPrefix;
+    private Integer tokenExpirationAfterDays;
+
+    public String getAuthorizationHeader() {
+        return HttpHeaders.AUTHORIZATION;
+    }
+}
